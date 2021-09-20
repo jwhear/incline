6e951f3c3a533f959f7cfa8715be6888cc60cc78
Justin Whear <justin@economicmodeling.com>
2021-09-20 15:06:58 -0700

    OK, don't make xml dynamic

--- build.zig
+++ build.zig
@@ -6,14 +6,14 @@ pub fn build(b: *std.build.Builder) void {
     const mode = b.standardReleaseOptions();
 
     // For now we'll rely on a simple XML parser from the vulkan-zig project
-    _ = b.exec(&[_][]const u8{
-        "curl", "-s",
-        "https://raw.githubusercontent.com/Snektron/vulkan-zig/master/generator/xml.zig",
-        "-o", "src/formats/xml.zig"
-    }) catch {
-        std.debug.print("Unable to download XML code\n", .{});
-        return;
-    };
+    //_ = b.exec(&[_][]const u8{
+        //"curl", "-s",
+        //"https://raw.githubusercontent.com/Snektron/vulkan-zig/master/generator/xml.zig",
+        //"-o", "src/formats/xml.zig"
+    //}) catch {
+        //std.debug.print("Unable to download XML code\n", .{});
+        //return;
+    //};
 
     const lib = b.addStaticLibrary("incline", "src/incline.zig");
     lib.setBuildMode(mode);
